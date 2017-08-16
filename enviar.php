c0b75f1115792662af8f1524ee3d1c347b72b84a
smartrix <rberrospi@smartrix.pe>
2017-08-15 21:18:46 -0500

    ee

--- enviar.php
+++ enviar.php
@@ -7,8 +7,8 @@ $id_provincia = $_POST['id_provincia'];
 $id_curso_imf = $_POST['id_curso_imf'];
 
 if(!$nombre == '' && !$apellidos == '' && !$email == '' && !$telefono == '' ){
-    $correo_admin = "cursos@institutodeseguridad.edu.pe";
-    $correo_web = "cursos@institutodeseguridad.edu.pe";
+    $correo_admin = "rberrospi@institutodeseguridad.edu.pe";
+    $correo_web = "rberrospi@institutodeseguridad.edu.pe";
     $titulo = 'Solicitud de: '.$nombre;
     $mensaje = '
     <table width="230px" border"0" cellspacing="0">
